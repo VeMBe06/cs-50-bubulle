a2e529e0ad7899c8c3ec451cf359b53c4ae1bb12
VeMBe06 <victor.mbaye14@gmail.com>
2023-01-07 19:19:16 +0100

    The bubble is working!

--- bubulle/static/index.js
+++ bubulle/static/index.js
@@ -1,1 +1,29 @@
-// alert("wesh ferot")
+
+
+// JS For the start-stop button
+// Select elements: start button, bubble
+var StartStop = document.getElementById("StartStop")
+var bubble= document.getElementById("Bubble")
+
+// Initialize with a first event listener
+StartStop.addEventListener("click", Start)
+
+// Start breathing, update event listeners
+function Start() {
+    bubble.classList.add("breathing")
+    StartStop.removeEventListener("click", Start)
+    StartStop.addEventListener("click", Stop)
+    StartStop.value = "Stop"
+
+}
+
+// Stop breathing, update event listeners
+function Stop() {
+    bubble.classList.remove("breathing")
+    StartStop.removeEventListener("click", Stop)
+    StartStop.addEventListener("click", Start)
+    StartStop.value = "Start"
+}
+
+
+
