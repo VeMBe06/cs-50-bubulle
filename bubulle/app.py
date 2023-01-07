a7140ba994fdb0c55c2f24388024ad95b5def7fd
VeMBe06 <victor.mbaye14@gmail.com>
2023-01-07 17:49:39 +0100

    big update full of stuff

--- bubulle/app.py
+++ bubulle/app.py
@@ -1,6 +1,7 @@
 # Import everything you need
 
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
+from flask_session import Session
 
 # Configure app
 app = Flask(__name__)
@@ -12,9 +13,24 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 if __name__ == "__main__":
     app.run(debug = True)
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 @app.route("/")
 def index():
-    """Show the main screen wiht bubble"""
+    """Show the main screen with bubble"""
 
     return render_template("index.html")
 
