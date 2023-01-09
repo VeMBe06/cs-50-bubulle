f23b58badc779a1f32eb62ebb9a46b60434eac58
VeMBe06 <victor.mbaye14@gmail.com>
2023-01-09 22:14:56 +0100

    completed the breathing parameters feature

--- bubulle/app.py
+++ bubulle/app.py
@@ -35,13 +35,33 @@ def index():
 
     return render_template("index.html")
 
-@app.route("/parameters")
+@app.route("/parameters", methods=["GET", "POST"])
 def parameters():
     """Change breathing parameters"""
     if request.method == "POST":
         # If we POST, we update the breathing parameters on CSS with JS
-        
-        return
+
+        # Obtain profile that was selected
+        profile = request.form.get("profile")
+        # Create switch statement to find which profile was chosen
+        # Do it by hand bc there's no real switch in python 3
+        if profile == "profile1":
+            # Create vars for animation-name | animation-duration | profile type
+            return render_template("index.html", anim_name="breathe1", anim_dur=6, prof_type="1:2:2")
+
+        elif profile == "profile2":
+            return render_template("index.html", anim_name="breathe2", anim_dur=9, prof_type="1:3:3")
+
+
+        elif profile == "profile3":
+            return render_template("index.html", anim_name="breathe3", anim_dur=14, prof_type="5:3:5")
+
+        elif profile == "profile4":
+            return render_template("index.html", anim_name="breathe4", anim_dur=17, prof_type="5:4:7")
+
+        else:
+            return render_template("index.html", anim_name="breathe3", anim_dur=14, prof_type="5:3:5")
+    
     
     else:
         # Else we just render the webpage
--- bubulle/static/index.js
+++ bubulle/static/index.js
@@ -28,40 +28,40 @@ function Stop() {
 
 
 
-// Set up breathing parameters buttons
-// First get buttons ids
-var profile1 = document.getElementById("profile1")
-var profile2 = document.getElementById("profile2")
-var profile3 = document.getElementById("profile3")
-var profile4 = document.getElementById("profile4")
+// // Set up breathing parameters buttons
+// // First get buttons ids
+// var profile1 = document.getElementById("profile1")
+// var profile2 = document.getElementById("profile2")
+// var profile3 = document.getElementById("profile3")
+// var profile4 = document.getElementById("profile4")
 
-// Then get .breathing class queryselector
-var breatheclass = document.querySelector(".breathing")
+// // Then get .breathing class queryselector
+// var breatheclass = document.querySelector(".breathing")
 
 
-// Add event listeners
-profile1.addEventListener("change", function() {
-    console.log("clicked on button 1")
-    breatheclass.style.animationName = "breathe1"
-    breatheclass.style.animationDuration = 6 + "s"
-})
+// // Add event listeners
+// profile1.addEventListener("change", function() {
+//     console.log("clicked on button 1")
+//     breatheclass.style.animationName = "breathe1"
+//     breatheclass.style.animationDuration = 6 + "s"
+// })
 
-profile2.addEventListener("change", function() {
-    breatheclass.style.animationName = "breathe2"
-    breatheclass.style.animationDuration = 9 + "s"
+// profile2.addEventListener("change", function() {
+//     breatheclass.style.animationName = "breathe2"
+//     breatheclass.style.animationDuration = 9 + "s"
 
-})
+// })
 
-profile3.addEventListener("change", function() {
-    breatheclass.style.animationName = "breathe3"
-    breatheclass.style.animationDuration = 14 + "s"
+// profile3.addEventListener("change", function() {
+//     breatheclass.style.animationName = "breathe3"
+//     breatheclass.style.animationDuration = 14 + "s"
 
-})
+// })
 
-profile4.addEventListener("change", function() {
-    breatheclass.style.animationName = "breathe4"
-    breatheclass.style.animationDuration = 17 + "s"
+// profile4.addEventListener("change", function() {
+//     breatheclass.style.animationName = "breathe4"
+//     breatheclass.style.animationDuration = 17 + "s"
 
-})
+// })
 
 
